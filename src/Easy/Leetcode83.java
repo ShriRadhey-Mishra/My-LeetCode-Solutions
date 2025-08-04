62a95011190c4e4707e870aa8c9e3a5a7116b8fa
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-04 23:21:18 +0530

    Removed duplicates in a singly linked list, Leetcode 83

---
+++ src/Easy/Leetcode83.java
@@ -0,0 +1,48 @@
+package Easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public void traverse() {
+        ListNode curr = this;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Leetcode83 {
+    public static void main(String[] args) {
+        Leetcode83 obj = new Leetcode83();
+        ListNode list = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3))));
+        ListNode setList = obj.deleteDuplicates(list);
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode newHead = head;
+        while (newHead != null && newHead.next != null) {
+            if (newHead.val == newHead.next.val) {
+                newHead.next = newHead.next.next;
+            } else {
+                newHead = newHead.next;
+            }
+        }
+//        head.traverse();
+        return head;
+    }
+}
