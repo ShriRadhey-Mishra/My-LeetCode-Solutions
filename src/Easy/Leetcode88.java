c904b59051480422bd8cb4f2f557c1749d04607a
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-08 23:14:16 +0530

    Leetcode 2620

--- src/Easy/Leetcode88.java
+++ src/Easy/Leetcode88.java
@@ -1,4 +1,23 @@
 package Easy;
 
+import java.util.Arrays;
+
 public class Leetcode88 {
+    public static void main(String[] args) {
+        Leetcode88 obj = new Leetcode88();
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+        System.out.println(Arrays.toString(nums1));
+        obj.merge(nums1, m, nums2, n);
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    }
+
+    public void insertOne(int m, int[] nums1, int[] nums2, int i) {
+
+    }
 }
