3ef78ce2a664adc65f376337d6d715f44bf4a505
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-09 22:56:51 +0530

    Leetcode 88- Used Three Pointer Method

--- src/Easy/Leetcode88.java
+++ src/Easy/Leetcode88.java
@@ -5,19 +5,30 @@ import java.util.Arrays;
 public class Leetcode88 {
     public static void main(String[] args) {
         Leetcode88 obj = new Leetcode88();
-        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums1 = {4, 5, 6, 0, 0, 0};
         int m = 3;
-        int[] nums2 = { 2, 5, 6 };
+        int[] nums2 = {1, 2, 3};
         int n = 3;
-        System.out.println(Arrays.toString(nums1));
+//        System.out.println(Arrays.toString(nums1));
         obj.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
     }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-    }
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-    public void insertOne(int m, int[] nums1, int[] nums2, int i) {
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
 
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
