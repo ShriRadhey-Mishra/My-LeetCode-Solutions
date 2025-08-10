d31977f1bd1477bc30a40dcb96f9ea8e2e151034
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-10 22:36:47 +0530

    Leetcode 7

---
+++ src/Medium/Leeetcode7.java
@@ -0,0 +1,24 @@
+package Medium;
+
+public class Leeetcode7 {
+    public static void main(String[] args) {
+        Leeetcode7 obj = new Leeetcode7();
+        int num = 123;
+        System.out.println(obj.reverse(num));
+    }
+    public int reverse(int x) {
+        long sign = 1;
+        if (x < 0) {
+            x *= -1;
+            sign = -1;
+        }
+        long reversed = 0;
+        while (x > 0) {
+            int lastDigit = x % 10;
+            reversed = (reversed * 10) + lastDigit;
+            x /= 10;
+        }
+        if (reversed >= Integer.MAX_VALUE || reversed <= Integer.MIN_VALUE) return 0;
+        return  Integer.parseInt(String.valueOf(reversed * sign));
+    }
+}
