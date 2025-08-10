5f5578cab49f85bb44da7208bee90baca999c8a4
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-11 02:29:47 +0530

    Leetcode 29 - Medium

--- src/Medium/Leetcode29.java
+++ src/Medium/Leetcode29.java
@@ -1,4 +1,38 @@
 package Medium;
 
 public class Leetcode29 {
+    public static void main(String[] args) {
+        int dividend = -2147483648;
+        int divisor = 1;
+        int result = divide(dividend, divisor);
+        System.out.println(result);
+    }
+    public static int divide(int dividend, int divisor) {
+//        Leetcode fucked this case up
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= temp+temp) {
+                temp+=temp;
+                multiple+=multiple;
+            }
+
+            dvd -= temp;
+            quotient += multiple;
+        }
+
+        return negative ? -quotient: quotient;
+    }
 }
