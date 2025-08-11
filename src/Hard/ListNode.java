24ec698ef1a00e1b55f7baed711dea7bad35b76b
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-11 14:34:58 +0530

    updated ListNode

--- src/Hard/ListNode.java
+++ src/Hard/ListNode.java
@@ -6,4 +6,12 @@ public class ListNode {
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public void traverse() {
+        ListNode curr = this;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
 }
