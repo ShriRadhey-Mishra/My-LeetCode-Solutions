e4b0c5934e43fe7c9d3089cff6b4ccdf33c47e4f
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-20 00:38:17 +0530

    Leetcode 287: Floyd's Loop Detection Algorithm

--- src/Medium/Leetcode287.java
+++ src/Medium/Leetcode287.java
@@ -1,4 +1,22 @@
 package Medium;
 
 public class Leetcode287 {
+    public static void main(String[] args) {
+        Leetcode287 obj = new Leetcode287();
+        int[] arr = new int[]{1,3,4,2,4};
+        System.out.println("Duplicate number is: " + obj.findDuplicate(arr));
+    }
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
 }
