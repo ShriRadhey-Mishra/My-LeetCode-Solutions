c8e0c4807d6d1da8e5c7dc67c0b94aeb680c0218
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-22 17:32:53 +0530

    Leetcode 374: Remember mid = start + (end-start )/2

--- src/Easy/Leetcode374.java
+++ src/Easy/Leetcode374.java
@@ -1,4 +1,35 @@
 package Easy;
 
 public class Leetcode374 {
+    public int pick = 10;
+
+    public static void main(String[] args) {
+        Leetcode374 obj = new Leetcode374();
+        System.out.println(obj.guessNumber(1000));
+    }
+
+    public int guessNumber(int n) {
+        if (guess(n) == 0) return n;
+        int start = 0;
+        int end = n;
+        int mid = start + (end - start) / 2;
+        int whereToMove = guess(mid);
+        do {
+            if (whereToMove == -1) {
+                end = mid;
+                mid = start + (end - start) / 2;
+                whereToMove = guess(mid);
+            } else if (whereToMove == 1) {
+                start = mid;
+                mid = start + (end - start) / 2;
+                whereToMove = guess(mid);
+            } else break;
+        } while (whereToMove != 0);
+        return mid;
+    }
+
+    public int guess(int num) {
+        if (num == pick) return 0;
+        return (num > pick) ? -1: 1;
+    }
 }
