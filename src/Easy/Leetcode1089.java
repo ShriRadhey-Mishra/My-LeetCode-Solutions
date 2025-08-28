cb755ab92e04912feb6484013a16b2cee0bf228d
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-28 22:48:17 +0530

    Leetcode 1089

---
+++ src/Easy/Leetcode1089.java
@@ -0,0 +1,24 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class Leetcode1089 {
+    public static void main(String[] args) {
+        Leetcode1089 obj = new Leetcode1089();
+        int[] arr = {1, 0, 2, 3, 0, 4, 5, 0};
+        System.out.println(Arrays.toString(arr));
+        obj.duplicateZeros(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0) {
+                for (int k = arr.length - 2; k > i; k--) {
+                    arr[k + 1] = arr[k];
+                }
+                arr[++i] = 0;
+            }
+        }
+    }
+}
