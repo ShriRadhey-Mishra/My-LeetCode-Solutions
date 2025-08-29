b2218ba1bfd980120d73b8ab4205d1c7057476c9
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-29 21:43:32 +0530

    better appraoch

--- src/Easy/Leetcode1089.java
+++ src/Easy/Leetcode1089.java
@@ -12,13 +12,22 @@ public class Leetcode1089 {
     }
 
     public void duplicateZeros(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == 0) {
-                for (int k = arr.length - 2; k > i; k--) {
-                    arr[k + 1] = arr[k];
-                }
-                arr[++i] = 0;
-            }
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            if (arr[i] == 0) {
+//                for (int k = arr.length - 2; k > i; k--) {
+//                    arr[k + 1] = arr[k];
+//                }
+//                arr[++i] = 0;
+//            }
+
+        int[] cpyArr = arr.clone();
+        int length = arr.length;
+        int index = 0;
+        for (int i = 0; i < length && index < length; i++) {
+            if (cpyArr[i] == 0) {
+                arr[index++] = cpyArr[i];
+                if (index < length) arr[index++] = 0;
+            } else arr[index++] = cpyArr[i];
         }
     }
 }
