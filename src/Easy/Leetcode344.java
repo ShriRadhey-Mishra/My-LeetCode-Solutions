e0516dad7531eff9c8a353d7b252e5b6eaf59ff6
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-29 22:52:32 +0530

    Leetcode 344

--- src/Easy/Leetcode344.java
+++ src/Easy/Leetcode344.java
@@ -1,4 +1,21 @@
 package Easy;
 
+import java.util.Arrays;
+
 public class Leetcode344 {
+    public static void main(String[] args) {
+        Leetcode344 obj = new Leetcode344();
+        char[] charArr = {'h','e','l','l','o'};
+        System.out.println(Arrays.toString(charArr));
+        obj.reverseString(charArr);
+        System.out.println(Arrays.toString(charArr));
+    }
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length/2; i++) {
+            int other = s.length-i-1;
+            char temp = s[i];
+            s[i] = s[other];
+            s[other] = temp;
+        }
+    }
 }
