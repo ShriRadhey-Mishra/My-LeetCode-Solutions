ba9e48198299a05842bf296d603ea84904b25f0f
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-09-03 23:15:21 +0530

    Leetcode 151

--- src/Medium/Leetcode151.java
+++ src/Medium/Leetcode151.java
@@ -1,4 +1,28 @@
 package Medium;
 
+import java.util.Arrays;
+
 public class Leetcode151 {
+    public static void main(String[] args) {
+        Leetcode151 obj = new Leetcode151();
+        String s = "          SKAI is  your    GOD     ";
+        System.out.println(obj.reverseWords(s));
+    }
+    public String reverseWords(String s) {
+        String[] str = s.strip().split(" ");
+        StringBuilder sb = new StringBuilder();
+        int left = 0, right = str.length-1;
+        while (left < right) {
+            String temp = str[left];
+            str[left++] = str[right];
+            str[right--] = temp;
+        }
+        for (int i = 0; i < str.length; i++) {
+            if (!str[i].isBlank()) {
+                sb.append(str[i]);
+                if (i != str.length-1) sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
