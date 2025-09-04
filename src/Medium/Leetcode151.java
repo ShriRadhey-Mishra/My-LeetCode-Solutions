b58708454f72ccbd102afb5c3d92b2b53d9c362a
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-09-04 20:06:30 +0530

    Updated the follow up approach

--- src/Medium/Leetcode151.java
+++ src/Medium/Leetcode151.java
@@ -1,7 +1,5 @@
 package Medium;
 
-import java.util.Arrays;
-
 public class Leetcode151 {
     public static void main(String[] args) {
         Leetcode151 obj = new Leetcode151();
@@ -9,20 +7,42 @@ public class Leetcode151 {
         System.out.println(obj.reverseWords(s));
     }
     public String reverseWords(String s) {
-        String[] str = s.strip().split(" ");
-        StringBuilder sb = new StringBuilder();
-        int left = 0, right = str.length-1;
-        while (left < right) {
-            String temp = str[left];
-            str[left++] = str[right];
-            str[right--] = temp;
-        }
-        for (int i = 0; i < str.length; i++) {
-            if (!str[i].isBlank()) {
-                sb.append(str[i]);
-                if (i != str.length-1) sb.append(" ");
+        char[] arr = s.toCharArray();
+        int read = 0, write = 0;
+
+        // skip leading spaces
+        // read from left until char is fown and write char from beginning
+        // add space only when new char is found
+        for (char ch: arr) {
+            if (Character.isSpaceChar(ch)) read++;
+            else {
+                arr[write++] = arr[read++];
+                if (read < arr.length && Character.isSpaceChar(arr[read])) arr[write++] = ' ';
             }
         }
-        return sb.toString();
+
+        // reverse the string till write
+        int start = 0, end = write-1;
+        while (start < end) {
+            char temp = arr[start];
+            arr[start++] = arr[end];
+            arr[end--] = temp;
+        }
+
+        // reverse individual words
+        start = 0;
+        for (int i =0 ; i < write; i++) {
+            if (Character.isSpaceChar(arr[i]) || i == write-1) {
+                end = (i == write-1)? i: i-1;
+                while (start < end) {
+                    char temp = arr[start];
+                    arr[start++] = arr[end];
+                    arr[end--] = temp;
+                }
+                start = i+1;
+            }
+        }
+
+        return new String(arr, 0, write).trim();
     }
 }
