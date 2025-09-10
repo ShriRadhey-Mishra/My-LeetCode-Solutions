074d85cbd2dfe987d664c5a9af163ac961067ca4
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-09-10 19:00:40 +0530

    Leetcode 237

--- src/Medium/Leetcode237.java
+++ src/Medium/Leetcode237.java
@@ -1,4 +1,16 @@
 package Medium;
 
 public class Leetcode237 {
+    public static void main(String[] args) {
+        Leetcode237 obj = new Leetcode237();
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        head.traverse();
+        obj.deleteNode(head.next.next);
+        head.traverse();
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
