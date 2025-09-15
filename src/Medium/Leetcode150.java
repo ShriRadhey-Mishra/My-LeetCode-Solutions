99610f8b9b59096fa50eea3b01adcf422c0af037
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-09-15 22:52:45 +0530

    Leetcode 150

--- src/Medium/Leetcode150.java
+++ src/Medium/Leetcode150.java
@@ -1,4 +1,38 @@
 package Medium;
 
+import java.util.Stack;
+
 public class Leetcode150 {
+    public static void main(String[] args) {
+        Leetcode150 obj = new Leetcode150();
+        String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        System.out.println(obj.evalRPN(tokens));
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String ex : tokens) {
+            switch (ex) {
+                case "+" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a + b);
+                }
+                case "-" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a - b);
+                }
+                case "*" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a * b);
+                }
+                case "/" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a / b);
+                }
+                default -> stack.push(Integer.parseInt(ex));
+            }
+
+        }
+        return stack.pop();
+    }
 }
