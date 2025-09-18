05ef9c8362e8a0564e196e8427413c5f05cab798
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-09-18 14:35:49 +0530

    Leetcode 3658

---
+++ src/Easy/Leetcode3658.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class Leetcode3658 {
+    public static void main(String[] args) {
+        Leetcode3658 obj = new Leetcode3658();
+        int n = 4;
+        System.out.println(obj.gcdOfOddEvenSums(n));
+    }
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
