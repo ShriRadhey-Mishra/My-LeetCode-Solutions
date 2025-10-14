882da03048c65a4935478a3994f3c1187facd5d7
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-10-15 02:04:13 +0530

    Updated Leetcode 198 - Dynamic Programming

--- src/Medium/Leetcode198.java
+++ src/Medium/Leetcode198.java
@@ -1,18 +1,25 @@
 package Medium;
 
+import java.util.HashMap;
+
 public class Leetcode198 {
+    public static HashMap<Integer, Integer> dp = new HashMap<>();
     public static void main(String[] args) {
         int[] houses = { 6, 7, 1, 30, 8, 2, 4};
         System.out.println(rob(houses));
     }
-    private static int houseRobber(int[] houses, int currentHouse) {
-        if (currentHouse>= houses.length) return 0;
-        int stealFirst = houses[currentHouse] + houseRobber(houses, currentHouse+2);
-        int skipFirst = houseRobber(houses, currentHouse+1);
-        return Math.max(stealFirst, skipFirst);
+    public static int houseRobber(int[] houses, int current) {
+        if (current >= houses.length) return 0;
+        if (!dp.containsKey(current)) {
+            int stealFirst = houses[current] + houseRobber(houses, current+2);
+            int skipFirst = houseRobber(houses, current+1);
+            dp.put(current, Math.max(stealFirst, skipFirst));
+        }
+        return dp.get(current);
     }
 
-    public static int rob(int[] houses) {
-        return houseRobber(houses, 0);
+    public static int rob(int[] nums) {
+        dp.clear();
+        return houseRobber(nums, 0);
     }
 }
