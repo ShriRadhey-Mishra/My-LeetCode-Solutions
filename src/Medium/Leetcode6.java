49447b05f59995a594e7316426f3815c9ae031d8
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-10-29 23:15:10 +0530

    Leetcode 6

---
+++ src/Medium/Leetcode6.java
@@ -0,0 +1,41 @@
+package Medium;
+
+public class Leetcode6 {
+    public static void main(String[] args) {
+        Leetcode6 obj = new Leetcode6();
+        String s = "PAYPALISHIRING";
+        int row = 3;
+        System.out.println(obj.convert(s, row));
+    }
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
