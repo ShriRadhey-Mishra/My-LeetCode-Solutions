eb372f2b46cdfbd880eaab65728e0bd3f638b669
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-11-09 21:20:10 +0530

    Leetcode 448

---
+++ src/Easy/Leetcode448.java
@@ -0,0 +1,30 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode448 {
+    public static void main(String[] args) {
+        Leetcode448 obj = new Leetcode448();
+        int[] arr  = {4,3,2,7,8,2,3,1};
+        System.out.println(obj.findDisappearedNumbers(arr));
+    }
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
